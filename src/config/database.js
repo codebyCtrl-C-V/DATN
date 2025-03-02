7f7d4923390373b49df1be9dd85fe47920f631d6
codebyCtrl-C-V <97162796+codebyCtrl-C-V@users.noreply.github.com>
2025-03-02 22:20:23 +0700

    config

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,17 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('datn', 'TRANDIEN', 'dien2004', { 
+const sequelize = new Sequelize(
+  process.env.DB_NAME,    
+  process.env.DB_USER,    
+  process.env.DB_PASS,
+  { 
   dialect: 'mysql',  
   host: 'localhost',  
   port: 3306,        
   logging: false,    
-});
+  }
+);
 
 (async () => {
   try {
